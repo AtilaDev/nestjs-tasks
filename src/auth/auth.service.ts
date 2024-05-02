6b871a781eed034821faa56dd25500567e17a560
Leandro <atiladevelop@gmail.com>
2024-05-01 22:06:06 -0300

    handling duplicate username

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UsersRepository } from './users.repository';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
@@ -8,4 +9,8 @@ export class AuthService {
     @InjectRepository(UsersRepository)
     private usersRepository: UsersRepository,
   ) {}
+
+  async singUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    await this.usersRepository.createUser(authCredentialsDto);
+  }
 }
